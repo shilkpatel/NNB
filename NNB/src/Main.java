98071c7c48562fbd2b75f1f18ec582d4da4945bd
Shil16 <65488613+Shil16@users.noreply.github.com>
2023-02-26 16:52:55 +0000

    ran it for 100000
    ran it for longer and it is slowly converging to the correct answer so these weights and biases are slightly more correct than the last ones

--- NNB/src/Main.java
+++ NNB/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         //network xor = new network();
         //xor.load_network("network.txt");
         test(xor);
-        for(int i=0;i<10000;i++)
+        for(int i=0;i<100000;i++)
         {
             //System.out.println(i);
             training(xor);
